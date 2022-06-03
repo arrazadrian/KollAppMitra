6171be0fbfed41e698c4678a261ad8a95ca89e62
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2022-06-03 21:37:44 +0700

    Sikit

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
-import Routernih from './src/Router/Index.js';
+import Routernih from './src/Router';
 
 const App = () => {
   return (
