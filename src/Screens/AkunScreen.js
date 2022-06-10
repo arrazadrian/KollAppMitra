5f0d2f22767a68f7a22dd5cfd8207143210f980f
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2022-06-10 20:26:27 +0700

    akun aman

--- src/Screens/AkunScreen.js
+++ src/Screens/AkunScreen.js
@@ -1,12 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Pressable } from 'react-native'
 import React from 'react'
-import { Kuning } from '../Utils/Warna'
+import { Ijo, IjoTua, Kuning, Putih} from '../Utils/Warna';
 
-const AkunScreen = () => {
+const AkunScreen = ({ navigation }) => {
   return (
-    <View style={styles.latar}>
-      <Text>AkunScreen Halo</Text>
-    </View>
+    <SafeAreaView style={styles.latar}>
+      <View style={styles.container}>
+            <View style={{flexDirection:'row', alignItems:'center', marginBottom: 10}}>
+                <View style={styles.foto}>
+                  <Text>Putuu</Text>
+                </View>
+                <View>
+                    <Text style={{fontSize: 30, fontWeight:'bold', color: Putih,}}>Arraz Adrian</Text>
+                    <Text style={{fontSize: 18,color: Putih,}}>Pelanggan</Text>
+                    <Pressable  onPress={() => navigation.push('EditScreen')} >
+                        <View style={styles.edit}>
+                          <Text style={{color: Putih, fontSize: 18, fontWeight:'bold'}}>Atur Profil</Text>
+                        </View>
+                    </Pressable>
+                </View>
+            </View>
+            <View style={{borderBottomColor: Ijo, borderBottomWidth: 1}}>
+              <Text style={{color: Putih, fontSize: 30, fontWeight: 'bold'}}>Info</Text>
+            </View>
+            <View style={{padding: 15}}>
+                <View style={{justifyContent:"space-between", marginBottom: 10}}>     
+                      <Text style={{color: Putih, fontSize: 15, fontWeight:'bold'}}>No.Handphone</Text> 
+                      <Text style={{color: Putih, fontSize: 20}}>0909090909090</Text>   
+                </View>
+                <View style={{justifyContent:"space-between", marginBottom: 10}}>     
+                      <Text style={{color: Putih, fontSize: 15, fontWeight:'bold'}}>Email</Text> 
+                      <Text style={{color: Putih, fontSize: 20}}>contoh@gmail.com</Text>   
+                </View>
+                <View style={{justifyContent:"space-between", marginBottom: 10}}>     
+                      <Text style={{color: Putih, fontSize: 15, fontWeight:'bold'}}>Alamat</Text> 
+                      <Text style={{color: Putih, fontSize: 20}}>Jl.Contoh aja</Text>   
+                </View>
+            </View>
+            <View style={styles.logout}>
+              <Text style={{fontSize: 20, color: Ijo, fontWeight: 'bold'}}>Keluar Akun</Text>
+            </View>
+      </View>
+    </SafeAreaView>
   )
 }
 
@@ -14,7 +49,44 @@ export default AkunScreen
 
 const styles = StyleSheet.create({
   latar:{
+    flex:1,
     backgroundColor: Kuning,
-    flex: 1,
+  },
+  foto:{
+    width: 100,
+    height: 100,
+    backgroundColor: Putih,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 30,
+  },
+  container:{
+    backgroundColor: IjoTua,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    height: '80%',
+    top: 140, 
+    padding: 20,
+  },
+  tulisan:{
+    fontSize: 18,
+    color: Putih,
+  },
+  edit:{
+    borderColor: Ijo,
+    borderWidth: 2,
+    borderRadius: 10,
+    width: '100%',
+    padding: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  logout:{
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    margin: 20,
   }
 })
--- src/Screens/EditScreen.js
+++ src/Screens/EditScreen.js
@@ -1,14 +1,84 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TextInput, View, Image, Pressable, TouchableWithoutFeedback, Keyboard } from 'react-native'
 import React from 'react'
+import { Ijo, IjoTua, Kuning, Putih } from '../Utils/Warna'
 
 const EditScreen = () => {
   return (
-    <View>
-      <Text>EditScreen</Text>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+    <View style={styles.latar}>
+            <View style={styles.atas}>
+                <View style={styles.gambar}>
+                  <Image/>
+                </View>
+                  <Text style={{color: Ijo, fontWeight: 'bold', fontSize: 20}}>Ganti Foto </Text>
+            </View>
+            <View style={{padding:10}}>
+                <Text style={styles.judulisi}>Nama</Text>
+                <TextInput style={styles.input} value="asiap"/>
+                <Text style={styles.judulisi}>No.Handphone</Text>
+                <TextInput style={styles.input} 
+                  keyboardType='numeric'
+                  value="08123456789"
+                  />
+                <Text style={styles.judulisi}>Email</Text>
+                <TextInput style={styles.input} value="asiap"/>
+                <Text style={styles.judulisi}>Alamat</Text>
+                <TextInput style={styles.input} value="asiap"/>
+            </View>
+            <Pressable style={styles.tombol}>
+              <Text style={styles.simpan}>Simpan</Text>
+            </Pressable>
     </View>
+    </TouchableWithoutFeedback>
   )
 }
 
 export default EditScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  latar:{
+    flex:1,
+    backgroundColor: Kuning,
+    padding: 20,
+  },
+  gambar:{
+    width: 100,
+    height: 100,
+    borderRadius: 20,
+    backgroundColor: Putih,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 10,
+    borderColor: IjoTua,
+    borderWidth: 1,
+  },
+  atas:{
+    alignItems: 'center',
+  },
+  input:{
+    borderBottomWidth: 2,
+    borderColor: Ijo,
+    marginBottom: 10,
+    fontSize: 20,
+  },
+  judulisi:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: IjoTua,
+  },
+  tombol:{
+    backgroundColor: Ijo,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    width: 120,
+    borderRadius: 10,
+    alignItems: 'center',
+    alignSelf: 'center',
+    marginTop: 20,
+  },
+  simpan:{
+    color: Putih, 
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+})
