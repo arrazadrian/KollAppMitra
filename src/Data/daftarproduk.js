a50ae672a906b431418a274c7b14e52aae7d1daf
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2022-06-15 17:44:18 +0700

    coba data

---
+++ src/Data/daftarproduk.js
@@ -0,0 +1,37 @@
+export const daftarproduk = 
+[{
+    id: 1, 
+    nama: 'Apel',
+    image: require('../assets/Image/apel.jpg'),
+    harga: 5000,
+    kuantitas: 300,
+    satuan:'g',
+    kategori:'Buah',
+},
+{ 
+    id: 2, 
+    nama: 'Jeruk',
+    image: require('../assets/Image/jeruk.jpg'),
+    harga: 6500,
+    kuantitas: 300,
+    satuan:'g',
+    kategori:'Buah',
+},
+{ 
+    id: 3, 
+    nama: 'Pisang',
+    image: require('../assets/Image/pisang.jpg'),
+    harga: 3500,
+    kuantitas: 500,
+    satuan:'g',
+    kategori:'Buah',
+},
+{ 
+    id: 4, 
+    nama: 'Mangga',
+    image: require('../assets/Image/mangga.jpg'),
+    harga: 7000,
+    kuantitas: 400,
+    satuan:'g',
+    kategori:'Buah',
+}]
