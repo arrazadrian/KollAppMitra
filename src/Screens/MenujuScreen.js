4c6bda817d1550ab7681e9b81be9216cccae69f2
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2022-07-01 16:59:29 +0700

    Menuju screen baru mulai

---
+++ src/Screens/MenujuScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MenujuScreen = () => {
+  return (
+    <View>
+      <Text>MenujuScreen</Text>
+    </View>
+  )
+}
+
+export default MenujuScreen
+
+const styles = StyleSheet.create({})
