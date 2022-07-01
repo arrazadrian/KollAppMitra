4fe9701a5f8d2218083551e70a4a9584f6d9060c
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2022-07-01 17:09:29 +0700

    ganti jadi otw

--- src/Screens/OtwScreen.js
+++ src/Screens/OtwScreen.js
@@ -1,14 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const MenujuScreen = () => {
+const OtwScreen = () => {
   return (
     <View>
-      <Text>MenujuScreen</Text>
+      <Text>OtwScreen</Text>
     </View>
   )
 }
 
-export default MenujuScreen
+export default OtwScreen
 
 const styles = StyleSheet.create({})
