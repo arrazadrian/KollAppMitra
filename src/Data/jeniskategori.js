03cd548a7a6486b094dfe03c9049457bde3de4df
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2022-07-04 00:14:55 +0700

    nambah data semua produk

--- src/Data/jeniskategori.js
+++ src/Data/jeniskategori.js
@@ -5,47 +5,53 @@ export const jeniskategori =
 [
     {
         id: 1, 
+        nama: 'Semua Produk',
+        image: require('../assets/Images/Gerobak.png'),
+
+    },
+    {
+        id: 2, 
         nama: 'Produk Laut',
         image: require('../assets/Images/Kategori01.png'),
 
     },
     {
-        id: 2, 
+        id: 3, 
         nama: 'Daging',
         image: require('../assets/Images/Kategori02.png'),
     },
     {
-        id: 3, 
+        id: 4, 
         nama: 'Buah',
         image: require('../assets/Images/Kategori03.png'),
     },
     {
-        id: 4, 
+        id: 5, 
         nama: 'Sayur',
         image: require('../assets/Images/Kategori04.png'),
     },
     {
-        id: 5, 
+        id: 6, 
         nama: 'Bahan Pokok',
         image: require('../assets/Images/Kategori06.png'),
     },
     {
-        id: 6, 
+        id: 7, 
         nama: 'Cemilan',
         image: require('../assets/Images/Kategori05.png'),
     },
     {
-        id: 7, 
+        id: 8, 
         nama: 'Lauk',
         image: require('../assets/Images/Kategori07.png'),
     },
     {
-        id: 8, 
+        id: 9, 
         nama: 'Bumbu',
         image: require('../assets/Images/Kategori08.png'),
     },
     {
-        id: 9, 
+        id: 10, 
         nama: 'Frozen Food',
         image: require('../assets/Images/Kategori09.png'),
     },
