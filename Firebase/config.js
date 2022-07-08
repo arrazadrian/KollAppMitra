f49c312c3aab4e791610e85efe5bf81c62b40bd7
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2022-07-08 09:43:38 +0700

    gas yuk

--- Firebase/config.js
+++ Firebase/config.js
@@ -1,23 +1,19 @@
-// // Import the functions you need from the SDKs you need
-// import firebase from "firebase/compat/app";
-// import { initializeApp } from "firebase/app";
-// import "firebase/compat/firestore";
-// import "firebase/compat/auth";
-// import "firebase/compat/storage";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBnMmAnAw0o2SZ9AyakgE450XblGfadi0U",
-//   authDomain: "koll-app.firebaseapp.com",
-//   projectId: "koll-app",
-//   storageBucket: "koll-app.appspot.com",
-//   messagingSenderId: "485979616959",
-//   appId: "1:485979616959:web:f451668063dd1cb896780a",
-//   measurementId: "G-L8D2C3B549"
-// };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBnMmAnAw0o2SZ9AyakgE450XblGfadi0U",
+  authDomain: "koll-app.firebaseapp.com",
+  projectId: "koll-app",
+  storageBucket: "koll-app.appspot.com",
+  messagingSenderId: "485979616959",
+  appId: "1:485979616959:web:f451668063dd1cb896780a",
+  measurementId: "G-L8D2C3B549"
+};
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
