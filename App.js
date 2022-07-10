582e28a5d55d9ba6f70a4e85a5e65657ef17a5e9
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2022-07-10 17:26:25 +0700

    tipis dikit

--- App.js
+++ App.js
@@ -2,8 +2,6 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import Routernih from './src/Router/Index.js';
-// import * as firebase from 'firebase';
-// import apiKeys from './Firebase/config';
 
 const App = () => {
   return (
