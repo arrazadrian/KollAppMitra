9d9cde0cbabe04fdcf05e4f833054c5d68c2f8f9
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2022-08-04 17:57:16 +0700

    bikin data buat receipt

--- src/Data/dataRiwayat.js
+++ src/Data/dataRiwayat.js
@@ -2,35 +2,29 @@ export const dataRiwayat =
 [
     {
         id: '1', 
-        nama: 'Apel',
-        deskripsi: 'Produk ini dibawa dengan penuh perhatian dan dijaga kesegarannya.',
-        image: require('./GambarData/apel.jpg'),
-        harga: '3500',
-        kuantitas: '360',
-        satuan:'g',
-        kategori:'Buah',
-        quantity: 0,
-    },
-    { 
-        id: '2', 
-        nama: 'Jeruk',
-        deskripsi: 'Produk ini dibawa dengan penuh perhatian dan dijaga kesegarannya.',
-        image: require('./GambarData/jeruk.jpg'),
-        harga: '4500',
-        kuantitas: '390',
-        satuan:'g',
-        kategori:'Buah',
-        quantity: 0,
-    },
-    { 
-        id: '3', 
-        nama: 'Pisang',
-        deskripsi: 'Produk ini dibawa dengan penuh perhatian dan dijaga kesegarannya.',
-        image: require('./GambarData/pisang.jpg'),
-        harga: '3500',
-        kuantitas: '520',
-        satuan:'g',
-        kategori:'Buah',
-        quantity: 0,
+        produk: [
+            {
+                jumlah : 2,
+                namaproduk: 'Bayam',
+                harga: '10000'
+            },
+            {
+                jumlah : 1,
+                namaproduk: 'pisang',
+                harga: '2000'
+            },
+            {
+                jumlah : 1,
+                namaproduk: 'Tempe',
+                harga: '8000'
+            },
+        ],
+        jml_produk: 4,
+        waktu: '10.15  Rabu, 10 Agustus 2022',
+        namatoko: 'Toko segar hijau',
+        id_transaksi: '41794y2g1d9141',
+        sub_total: '20000',
+        biaya_layanan: '2000',
+        hagatotal: '2200',
     },
 ]
