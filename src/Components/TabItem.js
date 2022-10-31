b3105ddadb7cb1fd77a0b6fd503fa19dd5b7d535
arrazadrian <arrazzaq_adrian@apps.ipb.ac.id>
2022-10-31 22:27:01 +0700

    nyoba callback di tab item

--- src/Components/TabItem.js
+++ src/Components/TabItem.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import {
    IconAkunIjo, IconAkunPutih,
    IconHomeIjo, IconHomePutih,
